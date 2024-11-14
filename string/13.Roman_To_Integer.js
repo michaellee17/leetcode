243af4eee06e89c666471b708c7e99da60c82414
michael <chunglilee17@gmail.com>
2024-11-14 17:22:08 +0800

    add:增加string

---
+++ string/13.Roman_To_Integer.js
@@ -0,0 +1,34 @@
+/**
+ * @param {string} s
+ * @return {number}
+ * 2024/11/14 第一次看 熟練度90%
+ * 思路:自製對應表 前後比較大小 從後面開始比
+ */
+const ROMANTOINTMAP = {
+  I: 1,
+  V: 5,
+  X: 10,
+  L: 50,
+  C: 100,
+  D: 500,
+  M: 1000,
+};
+
+var romanToInt = function (s) {
+  let total = 0;
+  let prevVal = 0;
+
+  for (let i = s.length - 1; i >= 0; i--) {
+    const currentVal = ROMANTOINTMAP[s[i]];
+    if (currentVal < prevVal) {
+      total -= currentVal;
+    } else {
+      total += currentVal;
+    }
+
+    prevVal = currentVal;
+  }
+  return total;
+};
+
+console.log(romanToInt("III")); //3
---
+++ string/14.Longest_Common_Prefix.js
@@ -0,0 +1,32 @@
+/**
+ * @param {string[]} strs
+ * @return {string}
+ * 2024/11/14 第一次做 掌握度70% 多次刪除
+ */
+var longestCommonPrefix = function (strs) {
+  if (strs.length === 0) return "";
+  if (strs.length === 1) return strs[0];
+  //假設第一個字串為prefix
+  let prefix = strs[0];
+  console.log(`output->prefix`, prefix);
+  //從第二個字串開始比較
+  for (let i = 1; i < strs.length; i++) {
+    //如果不是從頭開始的話就把prefix的最後一個字元拿掉
+    while (strs[i].indexOf(prefix) !== 0) {
+      //砍掉最後一個字元
+      prefix = prefix.substring(0, prefix.length - 1);
+      //若砍到長度變0就代表沒有共同字首 回傳空字串
+      if (prefix.length === 0) return "";
+    }
+  }
+
+  return prefix;
+};
+console.log(
+  `output->longestCommonPrefix(["flower", "flow", "flight"])`,
+  longestCommonPrefix(["flower", "flow", "flight"])
+);
+console.log(
+  `output->longestCommonPrefix(["dog","racecar","car"])`,
+  longestCommonPrefix(["dog", "racecardo", "cardo"])
+);
