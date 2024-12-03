4fa7985f67d6c7f29eaf02b4a5112514cc572c46
michael <chunglilee17@gmail.com>
2024-12-03 14:59:27 +0800

    feat:linked-list

---
+++ hash_table/141.Linked_List_Cycle.js
@@ -0,0 +1,31 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ * 2024/12/03 第一次看 掌握度60趴 快慢指针 了解linkedlist資料結構
+ */
+
+/**
+ * @param {ListNode} head
+ * @return {boolean}
+ */
+var hasCycle = function (head) {
+  if (head === null || head.next === null) {
+    return false;
+  }
+
+  let slow = head;
+  let fast = head.next;
+
+  while (slow !== fast) {
+    if (fast === null || fast.next === null) {
+      return false;
+    }
+    slow = slow.next;
+    fast = fast.next.next;
+  }
+
+  return true;
+};
