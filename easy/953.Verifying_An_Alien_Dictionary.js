3429897fe2e1d17eb561f044da9298b4a18deb42
michael <chunglilee17@gmail.com>
2025-07-24 17:52:31 +0800

    feat:953補說明

--- easy/953.Verifying_An_Alien_Dictionary.js
+++ easy/953.Verifying_An_Alien_Dictionary.js
@@ -3,6 +3,9 @@
  * @param {string} order
  * @return {boolean}
  * 2025/7/24 多層邏輯思考
+ * 1.先建立對照表用obj存取 方遍之後比較
+ * 2.比較相鄰字母 如果不同的話就比較 如果排序錯誤return false
+ * 3.若都比完沒問題的話 比較長度 w1 比 w2 長，則不合法(短的要排前面)
  */
 var isAlienSorted = function (words, order) {
   // 1. 建立字母順序對照表
